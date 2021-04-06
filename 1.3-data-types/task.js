b162c357552a77a209cbc312c7f271e5203c4ac9
wildcrow-sky <wildcrow@list.ru>
2021-04-06 17:12:17 +0300

    Lesson3 - 1.3-data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,22 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  let bodyOfCredit = amount - contribution; // Тело кредита
+  let monthlyRate = percent / 100 / 12; // Месячная процентная ставка
+  let numberOfMonths = (date.getFullYear() - new Date().getFullYear()) * 12 + date.getMonth() - new Date().getMonth(); // Количество месяцев
+
+  //let monthlyPayment = Math.abs((bodyOfCredit * (monthlyRate + monthlyRate / (Math.pow((1 + monthlyRate), numberOfMonths) - 1))).toFixed(2));
+  let monthlyPayment = bodyOfCredit * (monthlyRate + monthlyRate / (Math.pow((1 + monthlyRate), numberOfMonths) - 1));
+
+  let totalAmount = Math.abs((monthlyPayment * numberOfMonths).toFixed(2));
+  alert(`Общая сумма, которую необходимо выплатить клиенту = ${totalAmount} за ${numberOfMonths} месяцев!!!`);
+
+  return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  
+  let greeting = (Number(name) === 0 || name === undefined) ? `Привет, мир! Меня зовут Аноним.` : `Привет, мир! Меня зовут ${name}.`;
+  return greeting;
+
 }
