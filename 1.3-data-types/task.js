dfcc38d2f3a6f0495366d942545b0d679094f829
wildcrow-sky <wildcrow@list.ru>
2021-04-09 13:28:26 +0300

    Lesson3 - 1.3-data-types -  fixing of comments

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,22 +1,64 @@
 "use strict";
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  
+  percent = String(percent);
+  contribution = String(contribution);
+  amount = String(amount);
+
+  if (isNaN(percent)) { // Валидация процентов - текст и курсор не стоит
+    return `Параметр <percent> содержит неправильное значение <${percent}>`;
+  } else if (percent <= 0 || percent > 40) {
+    return `Параметр <percent> содержит неправильное значение <${percent}>`;
+  }
+  
+  if (isNaN(contribution)) { // Валидация первочначального взноса - текст и курсор не стоит
+    return `Параметр <contribution> содержит неправильное значение <${contribution}>`;
+  } else {
+    let lengthContribution = contribution.trim().length;
+    if (!lengthContribution) { // Делаем проверку на наличие "0" или ""
+      return `Параметр <contribution> содержит неправильное значение <${contribution}>`;
+    } else if (contribution < 0 || contribution > amount) { 
+      return `Параметр <contribution> содержит неправильное значение <${contribution}>`;
+    }
+  }
+  
+  if (isNaN(amount)) { // Валидация суммы кредита - текст и курсор не стоит
+    return `Параметр <amount> содержит неправильное значение <${amount}>`;
+  } else if (amount <= 0 || !Boolean(amount)) {
+    return `Параметр <amount> содержит неправильное значение <${amount}>`;
+  }
+  
+  if (isNaN(date)) { // Валидация даты окончания кредита (срок не менее месяца) - курсор не стоит
+    return `Параметр <date> содержит неправильное значение <${date}>`;
+  } else if (date <= new Date(new Date().setDate(new Date().getDate() + 31))) {
+    return `Параметр <date> содержит неправильное значение <${date}>`;
+  }
+
+  
+  percent = Number(percent);
+  contribution = Number(contribution);
+  amount = Number(amount);
+
   let bodyOfCredit = amount - contribution; // Тело кредита
   let monthlyRate = percent / 100 / 12; // Месячная процентная ставка
   let numberOfMonths = (date.getFullYear() - new Date().getFullYear()) * 12 + date.getMonth() - new Date().getMonth(); // Количество месяцев
-
-  //let monthlyPayment = Math.abs((bodyOfCredit * (monthlyRate + monthlyRate / (Math.pow((1 + monthlyRate), numberOfMonths) - 1))).toFixed(2));
-  let monthlyPayment = bodyOfCredit * (monthlyRate + monthlyRate / (Math.pow((1 + monthlyRate), numberOfMonths) - 1));
-
-  let totalAmount = Math.abs((monthlyPayment * numberOfMonths).toFixed(2));
-  alert(`Общая сумма, которую необходимо выплатить клиенту = ${totalAmount} за ${numberOfMonths} месяцев!!!`);
-
+  
+  let monthlyPayment = (bodyOfCredit * (monthlyRate + monthlyRate / (Math.pow((1 + monthlyRate), numberOfMonths) - 1))); // Расчет ежемесячного платежа
+  let totalAmount = Number((monthlyPayment * numberOfMonths).toFixed(2)); // Расчет общей суммы выплаты
+  
   return totalAmount;
 }
 
-function getGreeting(name) {
-  
-  let greeting = (Number(name) === 0 || name === undefined) ? `Привет, мир! Меня зовут Аноним.` : `Привет, мир! Меня зовут ${name}.`;
-  return greeting;
 
+function getGreeting(name) {
+  let name1;
+  
+  if (name === undefined || name.trim() == "") {
+    name1 = "Аноним";
+  } else {
+    name1 = name;
+  }
+  
+  return `Привет, мир! Меня зовут ${name1.trim()}.`;
 }
